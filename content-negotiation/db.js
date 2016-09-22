c440316f798d4acaa9a195b36e04ccf8a0cee0a6
leo <leo493852107@163.com>
2016-09-22 19:03:00 +0800

    finish content-negotiation example

---
+++ content-negotiation/db.js
@@ -0,0 +1,10 @@
+/**
+ * Created by leo on 22/09/2016.
+ */
+
+var users = [];
+users.push({ name: 'Tobi' });
+users.push({ name: 'Loki' });
+users.push({ name: 'Jane' });
+
+module.exports = users;
