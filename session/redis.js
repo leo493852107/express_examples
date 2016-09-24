be82d3aa127dcf796fe87c4b24adae6f12cab7b4
leo <leo493852107@163.com>
2016-09-24 11:06:59 +0800

    finish session example

---
+++ session/redis.js
@@ -0,0 +1,37 @@
+/**
+ * Created by leo on 24/09/2016.
+ */
+
+var express = require('express');
+var logger = require('morgan');
+var session = require('express-session');
+
+// pass the express to the connect redis module
+// allowing it to inherit from session.Store
+var RedisStore = require('connect-redis')(session);
+
+var app = express();
+
+app.use(logger('dev'));
+
+// Populates req.session
+app.use(session({
+    resave: false, // don't save session if unmodified
+    saveUninitialized: false, // don't create session until something stored
+    secret: 'keyboard cat',
+    store: new RedisStore
+}));
+
+app.get('/', function (req, res) {
+    var body = '';
+    if (req.session.views) {
+        ++req.session.views;
+    } else {
+        req.session.views = 1;
+        body += '<p>First time visiting? view this page in several browsers :)</p>';
+    }
+    res.send(body + '<p>viewd <strong>' + req.session.views + '</strong> times.</p>');
+});
+
+app.listen(3000);
+console.log('Express app started on port 3000');
